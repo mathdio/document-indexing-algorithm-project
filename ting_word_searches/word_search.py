f319e21372cf29294b0fef279b4e928c34089c40
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-06 18:30:53 -0300

    feat: R07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_list = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        file_dict = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for n in range(len(file["linhas_do_arquivo"])):
+            if file["linhas_do_arquivo"][n].lower().find(word.lower()) > -1:
+                file_dict["ocorrencias"].append({"linha": n + 1})
+
+        if len(file_dict["ocorrencias"]) > 0:
+            files_list.append(file_dict)
+
+    return files_list
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+print("teste".find("a"))
