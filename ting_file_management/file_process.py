01ceda07fc67728b8cd4bf74f83eecb0623678bd
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-06 15:50:08 -0300

    feat: R03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines_file = txt_importer(path_file)
+
+    for n in range(len(instance)):
+        if instance.search(n)['nome_do_arquivo'] == path_file:
+            return None
+
+    dict_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines_file),
+        "linhas_do_arquivo": lines_file,
+    }
+
+    instance.enqueue(dict_file)
+    print(dict_file, file=sys.stdout)
 
 
 def remove(instance):
