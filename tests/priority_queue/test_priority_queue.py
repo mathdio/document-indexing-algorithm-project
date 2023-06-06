24fc3407b73c4202281cc726dab9809a93152c89
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-06 17:39:17 -0300

    feat: R06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    assert len(queue) == 0
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(5)
+
+    queue.enqueue({"qtd_linhas": 5})
+    queue.enqueue({"qtd_linhas": 4})
+    assert len(queue) == 2
+
+    assert queue.regular_priority.data == [{"qtd_linhas": 5}]
+    assert queue.high_priority.data == [{"qtd_linhas": 4}]
+
+    assert queue.search(0) == {"qtd_linhas": 4}
+    assert queue.search(1) == {"qtd_linhas": 5}
+
+    queue.dequeue()
+    assert len(queue) == 1
+    queue.dequeue()
+    assert len(queue) == 0
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,24 +3,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__data = list()
+        self.data = list()
 
     def __len__(self):
-        return len(self.__data)
+        return len(self.data)
 
     def enqueue(self, value):
-        self.__data.append(value)
+        self.data.append(value)
 
     def dequeue(self):
-        removed_value = self.__data[0]
-        del self.__data[0]
+        removed_value = self.data[0]
+        del self.data[0]
         return removed_value
 
     def search(self, index):
-        if not 0 <= index < len(self.__data):
+        if not 0 <= index < len(self.data):
             raise IndexError("Índice Inválido ou Inexistente")
         else:
-            return self.__data[index]
+            return self.data[index]
 
     def __str__(self):
-        return f"{self.__data}"
+        return f"{self.data}"
