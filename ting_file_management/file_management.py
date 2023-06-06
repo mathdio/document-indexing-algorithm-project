3328af0771f36e4977eb844e3de60d70a79fec91
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-06 15:25:05 -0300

    feat: R02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return print('Formato inválido', file=sys.stderr)
+
+    else:
+        try:
+            with open(path_file, 'r') as file:
+                return [line[:-1]
+                        if line.endswith('\n')
+                        else line
+                        for line in file]
+
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
