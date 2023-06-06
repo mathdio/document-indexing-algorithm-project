9d8b7cbffaafaa23ecd01d901f0c9d11e0ea15ac
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-06 16:05:54 -0300

    feat: R05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,4 +29,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if not 0 <= position < len(instance):
+        print('Posição inválida', file=sys.stderr)
+    else:
+        sys.stdout.write(f"{instance.search(position)}")
